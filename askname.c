fe169df15f17d2af762892fba0f35979ed338a71
haryuuno21 <82949896+haryuuno21@users.noreply.github.com>
2026-09-11 15:19:23 +0300

    Rewrite comments

--- askname.c
+++ askname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+
+// Функция запроса имени и фамилии пользователя
 void askname(char *first, char *last)
 {
  printf("Enter your first name: ");
  fgets(first, 255, stdin);
- first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+ first[strlen(first)-1] = '\0'; /* Удаляем перенос строки в конце*/
  printf("Now enter your last name: ");
- gets(last); /* buffer overflow? what's that? */
+ gets(last);
 }
